00ad8beb87e97fc47a457ae97f40de7cc59d64b3
Luiz Fernando <d2022003915@unifei.edu.br>
2024-07-01 17:24:14 -0300

    Refatora definição de constantes de ambiente e verificações de integridade.

--- backend/src/constants/env.ts
+++ backend/src/constants/env.ts
@@ -1,15 +1,21 @@
-export const APP_NAME = 'TechLab Challenge 2024 3q Backend'
+export const APP_NAME = 'TechLab Challenge 2024 3q Backend';
 
-const APP_PORT_PROCESS_ARG: string | undefined = process.argv.find((arg, index, args) => (args[index - 1] === '--port' || args[index - 1] === '-p'))
+const appPortEnv = process.env.APP_PORT;
 
-if (APP_PORT_PROCESS_ARG && Number.isInteger(Number(APP_PORT_PROCESS_ARG))) process.env.APP_PORT = APP_PORT_PROCESS_ARG
+if (!appPortEnv) {
+  throw new Error('APP_PORT must be defined');
+}
 
-if (!process.env.APP_PORT) throw TypeError('APP_PORT must be defined')
+const parsedAppPort = parseInt(appPortEnv, 10);
 
-export const APP_PORT = Number(process.env.APP_PORT)
+if (!Number.isInteger(parsedAppPort)) {
+  throw new TypeError('APP_PORT must be an integer');
+}
 
-if (!Number.isInteger(APP_PORT)) throw TypeError('APP_PORT must be an integer')
+export const APP_PORT = parsedAppPort;
 
-if (!process.env.SECRET) throw TypeError('SECRET must be defined')
+if (!process.env.SECRET) {
+  throw new Error('SECRET must be defined');
+}
 
-export const SECRET = process.env.SECRET
+export const SECRET = process.env.SECRET;
